9856fcf813105fc898bbb8350cfc493cbbc902b1
9rmemo <achrafeljabbar@gmail.com>
2022-12-08 22:28:07 +0100

    open hive note box

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,5 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xff62FCD7);
+
+const kNotesBox = "notes_box";
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:note_app_hive_db/views/notes_view.dart';
 
-void main() {
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'constants.dart';
+
+Future<void> main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   runApp(const NoteApp());
 }
 
